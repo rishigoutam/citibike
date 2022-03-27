15a61e06a85e15ab30bde9d1ae11719d040750c0
rg <rishigoutam@users.noreply.github.com>
2022-03-27 14:38:49 -0700

    add convert dates flag to helpers.get_trips()
    - use new get_trips in eda notebooks

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -18,10 +18,13 @@ TRIPS_COLUMNS = [
 ]
 
 
-def get_trips(year: int, data_dir_rel_path="../data/") -> pd.DataFrame:
+def get_trips(
+    year: int, data_dir_rel_path="../data/", convert_dates=False
+) -> pd.DataFrame:
     """
     Returns trips for a given year
     :param year: the year for which to return trips for
+    :param convert_dates: whether to convert the `starttime` and `stoptime` date columns to `datetime64`
     :param data_dir_rel_path: the relative path to the data directory
     :return: dataframe of trips. NOTE: some years have mixed date types for the "starttime" and "stoptime" columns
     and for these, the type of the column will be left as string rather than datetime64
@@ -38,14 +41,14 @@ def get_trips(year: int, data_dir_rel_path="../data/") -> pd.DataFrame:
     trips.drop(trips.columns[0], axis=1, inplace=True)  # drop the dask index
 
     logging.debug(f"Changing dtypes")
-    __convert_trips_dtypes(trips, year)
+    __convert_trips_dtypes(trips, year, convert_dates)
 
     logging.debug(f"Done with {year}!")
 
     return trips
 
 
-def __convert_trips_dtypes(trips: pd.DataFrame, year: int) -> None:
+def __convert_trips_dtypes(trips: pd.DataFrame, year: int, convert_dates=False) -> None:
     # manually change dtype of columns for trips df as long as it is possible
     # starttime and stoptime have mixed formats for some years
     trips["tripduration"] = trips["tripduration"].astype("int32")
@@ -55,6 +58,10 @@ def __convert_trips_dtypes(trips: pd.DataFrame, year: int) -> None:
     trips["birthyear"] = trips["birthyear"].astype("float16").astype("int16")
     trips["gender"] = trips["gender"].astype("int8")
     trips["usertype"] = trips["usertype"].astype("category")
+
+    if convert_dates:
+        trips["starttime"] = trips["starttime"].astype("datetime64")
+        trips["stoptime"] = trips["stoptime"].astype("datetime64")
 
 
 def colors_from_values(values: pd.Series, palette_name: str, ascending=True):
