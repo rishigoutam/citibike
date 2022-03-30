ec5b60e364b32cb6e8345db6f9d644c34570b988
rg <rishigoutam@users.noreply.github.com>
2022-03-30 15:30:11 -0400

    app styling
    -add favicon
    -add document title
    -cleanup CSS (no more overflow)
    -station information colorbar on side

--- app/app.py
+++ app/app.py
@@ -12,92 +12,89 @@ import helpers
 
 ASSETS_DIR = "./assets/"
 
-# create iframes reading raw html from assets
+# styles
+tab_style = {"line-height": "3vh"}
+iframe_style = {"height": "600px", "width": "100%"}
+
+# create iframes reading raw html from asset
 with open(ASSETS_DIR + "allstations" + helpers.HTML_EXTENSION) as f:
     text = f.read()
-allstations_iframe = html.Div(
-    children=[html.Iframe(srcDoc=text, style={"height": "1067px", "width": "100%"})]
-)
+allstations_iframe = html.Div(children=[html.Iframe(srcDoc=text, style=iframe_style)])
 with open(ASSETS_DIR + "allstations-watercolor" + helpers.HTML_EXTENSION) as f:
     text = f.read()
 allstations_watercolor_iframe = html.Div(
-    children=[html.Iframe(srcDoc=text, style={"height": "1067px", "width": "100%"})]
+    children=[html.Iframe(srcDoc=text, style=iframe_style)]
 )
 
 with open(ASSETS_DIR + "topstations" + helpers.HTML_EXTENSION) as f:
     text = f.read()
 topstations_toner_iframe = html.Div(
-    children=[
-        html.Iframe(
-            srcDoc=text,
-            style={"height": "1067px", "width": "100%"},
-        )
-    ]
+    children=[html.Iframe(srcDoc=text, style=iframe_style)]
 )
 with open(ASSETS_DIR + "topstations-watercolor" + helpers.HTML_EXTENSION) as f:
     text = f.read()
 topstations_watercolor_iframe = html.Div(
-    children=[
-        html.Iframe(
-            srcDoc=text,
-            style={"height": "1067px", "width": "100%"},
-        )
-    ]
+    children=[html.Iframe(srcDoc=text, style=iframe_style)]
 )
 
 with open(ASSETS_DIR + "topstations-heatmap" + helpers.HTML_EXTENSION) as f:
     text = f.read()
-heatmap_toner_iframe = html.Div(
-    children=[
-        html.Iframe(
-            srcDoc=text,
-            style={"height": "1067px", "width": "100%"},
-        )
-    ]
-)
+heatmap_toner_iframe = html.Div(children=[html.Iframe(srcDoc=text, style=iframe_style)])
 with open(ASSETS_DIR + "topstations-heatmap-watercolor" + helpers.HTML_EXTENSION) as f:
     text = f.read()
 heatmap_watercolor_iframe = html.Div(
-    children=[
-        html.Iframe(
-            srcDoc=text,
-            style={"height": "1067px", "width": "100%"},
-        )
-    ]
+    children=[html.Iframe(srcDoc=text, style=iframe_style)]
 )
 
 # app
 app = dash.Dash(__name__)
 app.layout = html.Div(
     [
-        html.H1("Citibike Trips - Rebalance Analysis"),
+        html.Div(id="blank-output"),
         dcc.Tabs(
             id="tabs-toplevel",
             value="tab-1",
             children=[
-                dcc.Tab(label="All Stations", value="tab-1"),
-                dcc.Tab(label="Top Station Pairs", value="tab-2"),
-                dcc.Tab(label="Stations Heatmap)", value="tab-3"),
+                dcc.Tab(label="Station Distribution", value="tab-1", style=tab_style),
+                dcc.Tab(label="Station Information", value="tab-3", style=tab_style),
+                dcc.Tab(label="Top Rebalance Routes", value="tab-2", style=tab_style),
             ],
         ),
-        html.Div(id="tabs-firstlevel"),
+        html.Div(id="tabs-toplevel-children"),
     ]
 )
 
 
 @app.callback(
-    Output("tabs-firstlevel", "children"),
+    Output("tabs-toplevel-children", "children"),
     Input("tabs-toplevel", "value"),
 )
 def render_content(tab):
     if tab == "tab-1":
-        return allstations_iframe
+        return heatmap_watercolor_iframe
     elif tab == "tab-2":
         return topstations_toner_iframe
     elif tab == "tab-3":
-        return heatmap_watercolor_iframe
+        return allstations_iframe
 
 
-app.run_server(debug=False, use_reloader=False)
+# change title
+app.clientside_callback(
+    """
+    function(tab_value) {
+        if (tab_value === 'tab-1') {
+            document.title = 'Citibike Station Heatmap'
+        } else if (tab_value === 'tab-2') {
+            document.title = 'Station Information'
+        } else if (tab_value === 'tab-3') {
+            document.title = 'Top Routes by Rebalance Counts'
+        }
+    }
+    """,
+    Output("blank-output", "children"),
+    Input("tabs-toplevel", "value"),
+)
 
-#%%
+app.run_server(debug=False, use_reloader=False, port=8051)
+
+# %%
