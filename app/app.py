4f0b7e840182c84e29428040e0a3533273c3845f
rg <rishigoutam@users.noreply.github.com>
2022-03-30 17:39:13 -0400

    add dash app styling

--- app/app.py
+++ app/app.py
@@ -15,6 +15,20 @@ ASSETS_DIR = "./assets/"
 
 # styles
 iframe_style = {"height": "600px", "width": "100%"}
+# fixed sidebar on left. NOTE: hacky CSS widths
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "220px",
+    "padding": "1rem .5rem",
+    "background-color": "#f8f9fa",
+}
+# styles for the main content to position it to right of sidebar
+CONTENT_STYLE = {
+    "margin-left": "110px",
+}
 
 # create iframes reading raw html from asset
 with open(ASSETS_DIR + "allstations" + helpers.HTML_EXTENSION) as f:
@@ -48,7 +62,32 @@ heatmap_watercolor_iframe = html.Div(
 
 # app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
-app.layout = dbc.Container(
+
+# app sidebar
+sidebar = html.Div(
+    [
+        html.H3("Citibike", className="display-4"),
+        html.Hr(),
+        html.P(
+            "Improving operations through trip demand prediction and dock station rebalancing analysis",
+            className="lead",
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Demand for trips", href="/page-1", active="exact"),
+                dbc.NavLink("Rebalance analysis", href="/page-2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+# app content
+content = dbc.Container(id="page-content", style=CONTENT_STYLE)
+maps_content = dbc.Container(
     [
         dcc.Store(id="store"),
         html.Div(id="blank-output"),
@@ -62,10 +101,13 @@ app.layout = dbc.Container(
             active_tab="tab-1",
         ),
         html.Div(id="tab-content"),
-    ]
+    ],
 )
+# app layout
+app.layout = dbc.Container([dcc.Location(id="url"), sidebar, content])
 
 
+# tab callback
 @app.callback(
     Output("tab-content", "children"),
     Input("tabs", "active_tab"),
@@ -82,7 +124,7 @@ def render_tab_content(active_tab):
         return "No tab selected"
 
 
-# change title
+# document title callback
 app.clientside_callback(
     """
     function(active_tab) {
@@ -99,6 +141,26 @@ app.clientside_callback(
     Input("tabs", "active_tab"),
 )
 
-app.run_server(debug=True, use_reloader=False, port=8051)
+
+# sidebar navigation callback
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return html.P("Coming soon!")
+    elif pathname == "/page-1":
+        return html.P("Coming soon!")
+    elif pathname == "/page-2":
+        return maps_content
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+app.run_server(debug=True, use_reloader=False, port=8052)
 
 # %%
